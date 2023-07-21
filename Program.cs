0543212076509a66bda23188a5889f886e15d261
Dan Jacobson <dan@danj.ca>
2023-07-21 05:48:03 +0000

    Initial commit of TaskTracker CLI

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿var command = args[0];
+var commandArgs = args[1..];
+
+if (string.Equals(command, "record"))
+{
+    var taskName = commandArgs[0];
+    var score = int.Parse(commandArgs[1]);
+    Console.WriteLine($"You want to record the task \"{taskName}\" worth {score} points!");
+}
+else 
+{
+    Console.WriteLine($"Unrecognized command {command}");
+}
